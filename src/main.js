b325319c2c2de235b74d8dd8f875908e54a1b5e0
Gabriel Netto <gabrielcarlosnetto@outlook.com>
2026-02-17 14:51:59 -0300

    clear

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { configure } from "vue-gtag";
 import './style.css'
+
+configure({
+  tagId: "GTM-NM98W69S"
+})
+
 const app = createApp(App)
 
 app.use(router)
-
+app.use(gtag)
 app.mount('#app')
